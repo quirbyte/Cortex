20e0ac9db564a02e1b29a580567dd0bcdb9c7e14
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-02-19 00:01:42 +0530

    Added Create Tenant route

--- BackEnd/src/Routes/EventRoutes.ts
+++ BackEnd/src/Routes/EventRoutes.ts
@@ -1,10 +1,16 @@
 import { Router, Request, Response } from "express";
 import { TenantMiddleware } from "../Middleware/TenantMiddleware";
 import { EventModel } from "../Models/Event";
+import { userMiddleware } from "../Middleware/UserMiddleware";
 
 export const EventRouter = Router();
 
-EventRouter.post("/", TenantMiddleware, (req: Request, res: Response) => {});
+EventRouter.post(
+  "/",
+  userMiddleware,
+  TenantMiddleware,
+  (req: Request, res: Response) => {},
+);
 
 EventRouter.get("/", (req: Request, res: Response) => {});
 
@@ -12,10 +18,16 @@ EventRouter.get("/:id", (req: Request, res: Response) => {});
 
 EventRouter.get("/tenant/:tenantId", (req: Request, res: Response) => {});
 
-EventRouter.put("/:id", TenantMiddleware, (req: Request, res: Response) => {});
+EventRouter.put(
+  "/:id",
+  userMiddleware,
+  TenantMiddleware,
+  (req: Request, res: Response) => {},
+);
 
 EventRouter.delete(
   "/:id",
+  userMiddleware,
   TenantMiddleware,
   (req: Request, res: Response) => {},
 );
--- BackEnd/src/Routes/TenantRoutes.ts
+++ BackEnd/src/Routes/TenantRoutes.ts
@@ -8,7 +8,37 @@ const TenantRouter = Router();
 TenantRouter.post(
   "/create",
   userMiddleware,
-  (req: Request, res: Response) => {},
+  async (req: Request, res: Response) => {
+    try{
+      if(!req.userId){
+        return res.status(404).json({
+          msg: "User not verified!!",
+        });
+      }
+      let {name,slug} = req.body;
+      slug=slug.trim().toLowerCase().replace(/\s+/g, '-');
+      const sameSlug = await TenantModel.findOne({
+        slug:slug
+      });
+      if(sameSlug){
+        return res.status(409).json({
+          msg: "Slug already exists!!"
+        })
+      }
+      await TenantModel.create({
+        name:name,
+        slug:slug,
+        userId:req.userId
+      });
+      return res.json({
+        msg: "Created Tenant successfully!!"
+      })
+    }catch(e){
+      return res.status(500).json({
+        msg: "Failed to Create Tenant"
+      })
+    }
+  },
 );
 
 TenantRouter.get(
