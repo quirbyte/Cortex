b5e7d639a396609a4c0e67f9dfb5aaa550b584a7
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-02-19 21:24:09 +0530

    Added see any event bookers for tenant route

--- BackEnd/src/Models/Booking.ts
+++ BackEnd/src/Models/Booking.ts
@@ -3,6 +3,7 @@ import mongoose, { model, Document, Schema } from "mongoose";
 export interface InterfaceBooking extends Document {
   user_id: mongoose.Types.ObjectId;
   event_id: mongoose.Types.ObjectId;
+  checkedIn: boolean;
 }
 
 const BookingSchema = new Schema<InterfaceBooking>({
@@ -16,6 +17,10 @@ const BookingSchema = new Schema<InterfaceBooking>({
     ref:"Event",
     required: true,
   },
+  checkedIn:{
+    type:Boolean,
+    default: false
+  }
 },{timestamps:true});
 
 export const BookingModel = model<InterfaceBooking>("Booking",BookingSchema);
--- BackEnd/src/Routes/BookingRoutes.ts
+++ BackEnd/src/Routes/BookingRoutes.ts
@@ -1,28 +1,86 @@
 import { Router, Request, Response } from "express";
+import { Types } from "mongoose";
 import { userMiddleware } from "../Middleware/UserMiddleware";
 import { TenantMiddleware } from "../Middleware/TenantMiddleware";
 import { BookingModel } from "../Models/Booking";
+import { EventModel } from "../Models/Event";
 
 export const BookingRouter = Router();
 
 BookingRouter.post(
   "/book",
   userMiddleware,
-  (req: Request, res: Response) => {
-    
-  },
+  (req: Request, res: Response) => {},
 );
 
 BookingRouter.get(
   "/my-tickets",
   userMiddleware,
-  (req: Request, res: Response) => {},
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.userId) {
+        return res.status(401).json({
+          msg: "Authentication content missing!",
+        });
+      }
+      const userBookings = await BookingModel.find({
+        user_id: req.userId,
+      })
+        .populate({ path: "event_id", select: "name date venue time" })
+        .sort({ createdAt: -1 });
+      return res.json({
+        userBookings,
+        msg: "Fetched user bookings successfully!",
+      });
+    } catch (e) {
+      return res.status(500).json({
+        msg: "Internal server error on fetching user bookings",
+      });
+    }
+  },
 );
 
 BookingRouter.get(
   "/event/:eventId",
+  userMiddleware,
   TenantMiddleware,
-  (req: Request, res: Response) => {},
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.userId || !req.tenantId) {
+        return res.status(401).json({
+          msg: "Authentication/Tenant content missing!",
+        });
+      }
+      const EventFromReq = req.params.eventId;
+      if (!Types.ObjectId.isValid(EventFromReq as string)) {
+        return res.status(400).json({ msg: "Invalid ID format" });
+      }
+      const queryId = new Types.ObjectId(EventFromReq as string);
+      const event = await EventModel.findOne({
+        _id: queryId,
+        tenantId: req.tenantId,
+      });
+
+      if (!event) {
+        return res.status(403).json({
+          msg: "Access denied: This event does not belong to your tenant account.",
+        });
+      }
+      const bookingDetails = await BookingModel.find({
+        event_id: queryId,
+      })
+        .populate({ path: "user_id", select: "name email" })
+        .sort({ createdAt: -1 });
+      return res.json({
+        bookingDetails,
+        msg: "Event booking details fetched successfully!",
+      });
+    } catch (e) {
+      return res.status(500).json({
+        msg: "Internal server error on fetching event bookers!",
+      });
+    }
+  },
 );
 
 BookingRouter.post(
