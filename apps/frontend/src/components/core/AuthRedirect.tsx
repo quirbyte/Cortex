3a120b0628d50cf663a4234445a4948b3d7c8ea6
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-03-01 19:09:00 +0530

    fixing redirect routes and doing some local changes

--- apps/frontend/src/components/core/AuthRedirect.tsx
+++ apps/frontend/src/components/core/AuthRedirect.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function AuthRedirect(){
     const token = localStorage.getItem("token");
     if(token){
-        return <Navigate to="/dashboard" replace/>
+        return <Navigate to="/dashboard/overview" replace/>
     }
     return <Outlet/>
 }
